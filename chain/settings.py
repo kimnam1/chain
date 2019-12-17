82fe761956f8230a8045201e88155698faaa1673
nam12 <namil0721@gmail.com>
2019-12-17 19:56:54 +0900

    debug true

--- chain/settings.py
+++ chain/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')v_k$1@v3hb-u$3+8asy-g4a^2o=n4@k)w0vbadpk^=(w(3r3b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  False
+DEBUG = True
 
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
